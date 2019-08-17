a819888984609222c160529cdabeb9c75db7079b
donaldjohn <donaldjohn@qq.com>
2019-08-17 20:26:34 +0800

    Add P5

---
+++ src/main/java/P5LongestPalindrome.java
@@ -0,0 +1,10 @@
+public class P5LongestPalindrome {
+    public static String longestPalindrome(String s) {
+        char[] charArray= new char[100];
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
